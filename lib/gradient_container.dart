e271baeed2cc5548f5c51fe2dd8580617250dae2
JawadMM <almuttawa.j@hotmail.com>
2023-05-18 01:01:49 +0300

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:dice_app/white_text.dart';
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.firstColor, this.secondColor, {super.key});
+
+  final Color firstColor;
+  final Color secondColor;
+
+  final Color third = Colors.white;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          firstColor,
+          secondColor,
+        ], begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(
+        child: WhiteText('Hello Wordl!'),
+      ),
+    );
+    //this is a comment
+  }
+}
